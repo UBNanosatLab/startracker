ad466a179b01cac8fa5e2e7fdb00adfc16be4529
Your Name <aztennenbaum@gmail.com>
2016-05-03 00:52:05 -0400

    moved info.txt to info/starline.txt

--- catalog_gen/db_stats.sh
+++ catalog_gen/db_stats.sh
@@ -2,10 +2,11 @@
 source calibration/calibration.txt
 source calibration/dbsize.txt
 
-cut -d ',' -f 1 < info.txt | sed s:$:/$ARC_ERR:g|bc| sort -n | uniq -c| awk 'OFS="," {print $2,$1}'>data1.csv
-cut -d ',' -f 2 < info.txt | sed s:$:/$ARC_ERR:g|bc| sort -n | uniq -c| awk 'OFS="," {print $2,$1}'>data2.csv
-cut -d ',' -f 3 < info.txt | sed s:$:/$ARC_ERR:g|bc| sort -n | uniq -c| awk 'OFS="," {print $2,$1}'>data3.csv
+cd info
+cut -d ',' -f 1 < starline.txt | sed s:$:/$ARC_ERR:g|bc| sort -n | uniq -c| awk 'OFS="," {print $2,$1}'>param1.csv
+cut -d ',' -f 2 < starline.txt | sed s:$:/$ARC_ERR:g|bc| sort -n | uniq -c| awk 'OFS="," {print $2,$1}'>param2.csv
+cut -d ',' -f 3 < starline.txt | sed s:$:/$ARC_ERR:g|bc| sort -n | uniq -c| awk 'OFS="," {print $2,$1}'>param3.csv
 
-cut -d ',' -f 1 < info.txt | sed s:$:%\($ARC_ERR*$PARAM1\)/$ARC_ERR:g|bc| sort -n | uniq -c| awk 'OFS="," {print $2,$1}'>data4.csv
-cut -d ',' -f 2 < info.txt | sed s:$:%\($ARC_ERR*$PARAM2\)/$ARC_ERR:g|bc| sort -n | uniq -c| awk 'OFS="," {print $2,$1}'>data5.csv
-cut -d ',' -f 3 < info.txt | sed s:$:%\($ARC_ERR*$PARAM3\)/$ARC_ERR:g|bc| sort -n | uniq -c| awk 'OFS="," {print $2,$1}'>data6.csv
+cut -d ',' -f 1 < starline.txt | sed s:$:%\($ARC_ERR*$PARAM1\)/$ARC_ERR:g|bc| sort -n | uniq -c| awk 'OFS="," {print $2,$1}'>param1_wrapped.csv
+cut -d ',' -f 2 < starline.txt | sed s:$:%\($ARC_ERR*$PARAM2\)/$ARC_ERR:g|bc| sort -n | uniq -c| awk 'OFS="," {print $2,$1}'>param2_wrapped.csv
+cut -d ',' -f 3 < starline.txt | sed s:$:%\($ARC_ERR*$PARAM3\)/$ARC_ERR:g|bc| sort -n | uniq -c| awk 'OFS="," {print $2,$1}'>param3_wrapped.csv
