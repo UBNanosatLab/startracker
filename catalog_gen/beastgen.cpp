f7764cc91afdf880ab4ac1f7faf700e7dfd5c117
Andrew Tennenbaum <aztennenbaum@gmail.com>
2016-07-21 17:59:14 -0400

    pointer weirdness. I give up for now

--- catalog_gen/beastgen.cpp
+++ catalog_gen/beastgen.cpp
@@ -1,9 +1,9 @@
-#include <iostream>     // cout
+#include <iostream>	 // cout
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-#include <fcntl.h>    /* For O_RDWR */
+#include <fcntl.h>	/* For O_RDWR */
 #include <sys/mman.h>
 
 
@@ -22,118 +22,123 @@ void add_entry(int mapidx,int curr_const) {
 	*staridx=curr_const;
 }
 
-double ARC_ERR;
-int PARAM1,PARAM2,PARAM3,NUMCONST;
-int i1_max,i2_max,i3_max;
-
-//i1..3 = int(p1..3+1)/2
-
-
 int main (int argc, char** argv) {
 	//load the stardb
-    if (argc!=6){
-        cout<<"Usage: ./beastgen PARAM1 PARAM2 PARAM3 NUMCONST ARC_PER_PIX < info/starline.txt"<<endl;
-        exit(0);
-    }
-    int PARAM1=atoi(argv[1]);
-    int PARAM2=atoi(argv[2]);
-    int PARAM3=atoi(argv[3]);
-    int NUMCONST=atoi(argv[4]);
-    int ARC_ERR=atof(argv[5]);
+	if (argc!=6){
+		cout<<"Usage: ./beastgen PARAM1 PARAM2 PARAM3 NUMCONST ARC_PER_PIX < info/starline.txt"<<endl;
+		exit(0);
+	}
+	int PARAM1=atoi(argv[1]);
+	int PARAM2=atoi(argv[2]);
+	int PARAM3=atoi(argv[3]);
+	int NUMCONST=atoi(argv[4]);
+	double ARC_ERR=atof(argv[5]);
 
-    int i1_max=PARAM1/2;
-    int i2_max=PARAM2/2;
-    int i3_max=PARAM3/2;
+	int i1_max=PARAM1/2;
+	int i2_max=PARAM2/2;
+	int i3_max=PARAM3/2;
 
-    size_t mapsize = i1_max*i2_max*i3_max;
-    size_t starptrsize = NUMCONST;
-    size_t dbsize = mapsize*sizeof(void*) + starptrsize*sizeof(struct constelation);
-    
-    /* Open a file for writing.
-     *  - Creating the file if it doesn't exist.
-     *  - Truncating it to 0 size if it already exists. (not really needed)
-     *
-     * Note: "O_WRONLY" mode is not sufficient when mmaping.
-     */
+	size_t mapsize = i1_max*i2_max*i3_max;
+	size_t dbsize = mapsize*sizeof(int) + NUMCONST*sizeof(struct constelation);
+	
+	/* Open a file for writing.
+	 *  - Creating the file if it doesn't exist.
+	 *  - Truncating it to 0 size if it already exists. (not really needed)
+	 *
+	 * Note: "O_WRONLY" mode is not sufficient when mmaping.
+	 */
 
-    const char *filepath = "beastdb.bin";
-    int fd = open(filepath, O_RDWR | O_CREAT | O_TRUNC, (mode_t)0600);
-    if (fd == -1) {
-        perror("Error opening file for writing");
-        exit(EXIT_FAILURE);
-    }
+	const char *filepath = "beastdb.bin";
+	int fd = open(filepath, O_RDWR | O_CREAT | O_TRUNC, (mode_t)0600);
+	if (fd == -1) {
+		perror("Error opening file for writing");
+		exit(EXIT_FAILURE);
+	}
 
-    // Stretch the file size to the size of the (mmapped) array of char
+	// Stretch the file size to the size of the (mmapped) array of char
 
 
-    if (lseek(fd, dbsize-1, SEEK_SET) == -1) {
-        close(fd);
-        perror("Error calling lseek() to 'stretch' the file");
-        exit(EXIT_FAILURE);
-    }
-    
-    if (write(fd, "", 1) == -1) {
-        close(fd);
-        perror("Error writing last byte of the file");
-        exit(EXIT_FAILURE);
-    }
-    
+	if (lseek(fd, dbsize-1, SEEK_SET) == -1) {
+		close(fd);
+		perror("Error calling lseek() to 'stretch' the file");
+		exit(EXIT_FAILURE);
+	}
+	
+	if (write(fd, "", 1) == -1) {
+		close(fd);
+		perror("Error writing last byte of the file");
+		exit(EXIT_FAILURE);
+	}
+	
 
-    // Now the file is ready to be mmapped.
+	// Now the file is ready to be mmapped.
 
-    map = (int*)mmap(0, dbsize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    starptr=(struct constelation*)(map+mapsize*sizeof(void*));
+	//map = (int*)mmap(0, dbsize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	//void *maptemp = malloc(dbsize);
+	int poop[47249800];
+	void *maptemp = &poop;
+	map=(int*)maptemp;
+	struct constelation *starptrtemp=(struct constelation*)(&map[mapsize]);
+	starptr=starptrtemp;
+	/*
+	if (map == MAP_FAILED)
+	{
+		close(fd);
+		perror("Error mmapping the file");
+		exit(EXIT_FAILURE);
+	}
+	*/
+	memset(maptemp, -1, dbsize);
+	cout<<map[0]<<endl<<flush;
+	cout<<starptrtemp[0].last<<endl<<flush;
+	cout<<map[0]<<endl<<flush;
+	for (int curr_const=0;curr_const<NUMCONST;curr_const++){
+		double temp1;
+		for (int i=0;i<6;i++) {
+			cin>>temp1;
+			starptrtemp[curr_const].p[i]=temp1;
+			cout<<starptrtemp[curr_const].p[i]<<flush;
+		}
+		for (int i=0;i<4;i++) {
+			cin>>starptr[curr_const].s[i];
+			cout<<starptr[curr_const].s[i]<<flush;
+		}
+		int i1=(int)(starptr[curr_const].p[0]/ARC_ERR+.5)%PARAM1;i1=i1/2+i1&1;
+		int i2=(int)(starptr[curr_const].p[1]/ARC_ERR+.5)%PARAM2;i2=i2/2+i2&1;
+		int i3=(int)(starptr[curr_const].p[2]/ARC_ERR+.5)%PARAM3;i3=i3/2+i3&1;
 
-    
-    if (map == MAP_FAILED)
-    {
-        close(fd);
-        perror("Error mmapping the file");
-        exit(EXIT_FAILURE);
-    }
-    memset(map, -1, dbsize);
-    
-    for (int curr_const=0;curr_const<NUMCONST;curr_const++){
-	for (int i=0;i<6;i++) cin>>starptr[curr_const].p[i];
-	for (int i=0;i<4;i++) cin>>starptr[curr_const].s[i];
-	int i1=(int)(starptr[curr_const].p[0]/ARC_ERR+.5)%PARAM1;i1=i1/2+i1&1;
-	int i2=(int)(starptr[curr_const].p[1]/ARC_ERR+.5)%PARAM2;i2=i2/2+i2&1;
-	int i3=(int)(starptr[curr_const].p[2]/ARC_ERR+.5)%PARAM3;i3=i3/2+i3&1;
-
-    int i1_l=((i1-1)%i1_max)*i2_max*i3_max;
-    int i1_u=((i1)%i1_max)*i2_max*i3_max;
-    int i2_l=((i2-1)%i2_max)*i3_max;
-    int i2_u=((i2)%i2_max)*i3_max;
-    int i3_l=((i3-1)%i3_max);
-    int i3_u=((i3)%i3_max);
-        
-    
-    add_entry(i1_l+i2_l+i3_l,curr_const);
-    add_entry(i1_u+i2_l+i3_l,curr_const);
-    add_entry(i1_l+i2_u+i3_l,curr_const);
-    add_entry(i1_u+i2_u+i3_l,curr_const);
-    add_entry(i1_l+i2_l+i3_u,curr_const);
-    add_entry(i1_u+i2_l+i3_u,curr_const);
-    add_entry(i1_l+i2_u+i3_u,curr_const);
-    add_entry(i1_u+i2_u+i3_u,curr_const);
-
-    }
-    
-    
-    // Write it now to disk
-    if (msync(map, dbsize, MS_SYNC) == -1) {
-        perror("Could not sync the file to disk");
-    }
-    
-    // Don't forget to free the mmapped memory
-    if (munmap(map, dbsize) == -1) {
-        close(fd);
-        perror("Error un-mmapping the file");
-        exit(EXIT_FAILURE);
-    }
-
-    // Un-mmaping doesn't close the file, so we still need to do that.
-    close(fd);
+		int i1_l=((i1-1)%i1_max)*i2_max*i3_max;
+		int i1_u=((i1)%i1_max)*i2_max*i3_max;
+		int i2_l=((i2-1)%i2_max)*i3_max;
+		int i2_u=((i2)%i2_max)*i3_max;
+		int i3_l=((i3-1)%i3_max);
+		int i3_u=((i3)%i3_max);
+		
+		add_entry(i1_l+i2_l+i3_l,curr_const);
+		add_entry(i1_u+i2_l+i3_l,curr_const);
+		add_entry(i1_l+i2_u+i3_l,curr_const);
+		add_entry(i1_u+i2_u+i3_l,curr_const);
+		add_entry(i1_l+i2_l+i3_u,curr_const);
+		add_entry(i1_u+i2_l+i3_u,curr_const);
+		add_entry(i1_l+i2_u+i3_u,curr_const);
+		add_entry(i1_u+i2_u+i3_u,curr_const);
+	}
+	
+	/*
+	// Write it now to disk
+	if (msync(map, dbsize, MS_SYNC) == -1) {
+		perror("Could not sync the file to disk");
+	}
+	
+	// Don't forget to free the mmapped memory
+	if (munmap(map, dbsize) == -1) {
+		close(fd);
+		perror("Error un-mmapping the file");
+		exit(EXIT_FAILURE);
+	}
+	*/
+	// Un-mmaping doesn't close the file, so we still need to do that.
+	close(fd);
 	
 }
 
