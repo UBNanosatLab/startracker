05a32512e616a5a0cb632243d3f7c96ad203d0eb
Andrew Tennenbaum <aztennenbaum@gmail.com>
2016-10-02 02:08:36 -0400

    - Converted all of the code to apply the image distortion correction calculated by astrometry, using astopy wcs library - this has the side effect of converting all pixel x,y coordinates into focal plane coordinates, which has 0,0 at the focal point of the sensor rather than at the top left corner of the image. This actually provides a much more natural system for conversion to x,y,z so I have converted beast, and everything else to use it
    The distortion correction results in more stars detected

--- beast/gen_test_px.py
+++ beast/gen_test_px.py
@@ -5,20 +5,31 @@ sys.path.append('../catalog_gen/')
 os.chdir('../catalog_gen/')
 
 from gendb import *
+from astropy import wcs
 
 filterunreliable()
-filtermagnitude()
+filterbrightness()
 filterdoublestars()
 
-ra=116.321270769
-dec=86.5582328937
-fovradius=4.545 / 2
-orientation=119.27164
-#orientation=180
+ra=93.2341656425
+dec=86.2682656958
+
+ra_center=116.321270769
+dec_center=86.5582328937
 
 x=math.cos(math.radians(ra))*math.cos(math.radians(dec))
 y=math.sin(math.radians(ra))*math.cos(math.radians(dec))
 z=math.sin(math.radians(dec))
+
+
+x_center=math.cos(math.radians(ra_center))*math.cos(math.radians(dec_center))
+y_center=math.sin(math.radians(ra_center))*math.cos(math.radians(dec_center))
+z_center=math.sin(math.radians(dec_center))
+
+fovradius=4.545 / 2
+orientation=119.27164
+#orientation=180
+
 
 def rotation_matrix(axis, theta):
     """
@@ -38,13 +49,18 @@ def rotation_matrix(axis, theta):
 sd=np.array(stardb.values(),dtype = object)
 xyz = spatial.cKDTree(np.array(sd[:,4:7].tolist(),dtype=float))
 
-result=sd[xyz.query_ball_point([x,y,z],2*abs(math.sin(math.radians(fovradius)/2)))]
+result=sd[xyz.query_ball_point([x_center,y_center,z_center],2*abs(math.sin(math.radians(fovradius)/2)))]
 
 result[:,4:7] = np.transpose(np.dot(rotation_matrix([0, 0, 1], math.radians(-ra)), np.transpose(result[:,4:7])))
 result[:,4:7] = np.transpose(np.dot(rotation_matrix([0, 1, 0], math.radians(dec)), np.transpose(result[:,4:7])))
-result[:,4:7] = np.transpose(np.dot(rotation_matrix([1, 0, 0], math.radians(orientation + 90)), np.transpose(result[:,4:7])))
 
-result[:,5:6]=(IMG_X/2)*(1+(result[:,5:6]/result[:,4:5])/np.tan(DEG_X*np.pi/(180*2)))
-result[:,6:7]=(IMG_Y/2)*(1-(result[:,6:7]/result[:,4:5])/np.tan(DEG_Y*np.pi/(180*2)))
+#center_xyz=[x_center,y_center,z_center]
+#center_xyz=np.dot(rotation_matrix([0, 0, 1], math.radians(-ra)), center_xyz)
+#center_xyz=np.dot(rotation_matrix([0, 1, 0], math.radians(dec)), center_xyz)
+#print center_xyz
+result[:,4:7] = np.transpose(np.dot(rotation_matrix([1,0,0], math.radians(orientation)), np.transpose(result[:,4:7])))
+
+result[:,5:6]=(result[:,5:6]/result[:,4:5])*(IMG_X/2)/np.tan(DEG_X*np.pi/(180*2))
+result[:,6:7]=(result[:,6:7]/result[:,4:5])*(IMG_Y/2)/np.tan(DEG_Y*np.pi/(180*2))
 for i in result:
 	print i[5],i[6],i[7]
