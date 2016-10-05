3ecca30bddea1a498f660dccb238c07c4c9f83fa
mattrant <mattrant@buffalo.edu>
2016-10-05 08:42:11 -0400

    Speeds up extract_stars by a factor of 10
    The center pixel of the star is used as an approximation of the
    brightest pixel. This action much quicker than finding the actual brightest
    pixel by generating the star histogram and then looping through to find
    the highest non-zero intensity in the histogram array. This also cuts out
    the overhead from masking the image which was a sizeable portion of
    the original computation time.
    Also fixes the placement of the docstring so the docstring will show
    when calling extract_stars.__doc__

--- beast/getstars.py
+++ beast/getstars.py
@@ -9,21 +9,21 @@ execfile("../catalog_gen/calibration/calibration.txt")
 #see http://docs.astropy.org/en/stable/api/astropy.wcs.WCS.html
 wcslist = fits.open('../catalog_gen/calibration/image.wcs')
 w = wcs.WCS(wcslist[0].header)
-"""
-Takes in a picture and will extract the star x,y centroids as well as
-the value of the brightest pixel in each star
-
-Args:
-    input_file: the file to be processed
-Returns:
-    An array containing tuples of star info. Tuple format :(x,y,brightest value in star)
-
-"""
 def extract_stars(input_file):
-    img = cv2.imread(input_file)
-    gray_scale = cv2.cvtColor( img, cv2.COLOR_RGB2GRAY );
+    """
+    Takes in a picture and will extract the star x,y centroids as well as
+    the value of the brightest pixel in each star
+
+    Args:
+        input_file: the file to be processed
+    Returns:
+        An array containing tuples of star info. Tuple format :(x,y,brightest value in star)
+
+    """
+    # 0 is the value used to read in an image in grey scale
+    img = cv2.imread(input_file,0)
     #removes areas of the image that don't meet our brightness threshold
-    ret,thresh = cv2.threshold(gray_scale,IMAGE_MEAN+IMAGE_STDEV*BRIGHT_ERR_SIGMA,IMAGE_MAX,3)
+    ret,thresh = cv2.threshold(img,IMAGE_MEAN+IMAGE_STDEV*BRIGHT_ERR_SIGMA,IMAGE_MAX,3)
     contours,heirachy = cv2.findContours(thresh,1,2);
     stars = []
     for c in contours:
@@ -32,18 +32,11 @@ def extract_stars(input_file):
             #this is how the x and y position are defined by cv2
             cx = M['m10']/M['m00']
             cy = M['m01']/M['m00']
+            #the center pixel is used as the approximation of the brightest pixel
+            #in the star in order to speed up the function by a factor of 10 and still
+            #have beast return matches for constellations
+            stars.append((cx,cy,img[cy,cx]))
 
-            #these create a mask for the selected star that we are processing
-            mask2 = np.zeros(gray_scale.shape[:2],np.uint8)
-            cv2.drawContours(mask2,[c],-1,(255,255,255),3)
-
-            hist = cv2.calcHist([img],[0],mask2,[256],[0,256])
-            #finds the highest non-zero index in the histogram
-            brightest_pixel = 0
-            for i in range(len(hist)):
-                if hist[i][0] !=0:
-                    brightest_pixel = i
-            stars.append((cx,cy,brightest_pixel))
     #use astrometry calibration data to correct for image distortion
     #see http://docs.astropy.org/en/stable/api/astropy.wcs.WCS.html
     results=np.array(stars)
