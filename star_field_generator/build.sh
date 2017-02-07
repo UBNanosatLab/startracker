9e330abca76405ad38f957a3dfc7abfd69d498e6
Andrew Tennenbaum <aztennenbaum@gmail.com>
2017-02-07 02:41:14 -0500

    added auto texture rebuild script

---
+++ star_field_generator/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+for i in 1 2 3 4 6 8; do ./generate_cube.sh $i; echo $i; done
+ 
--- star_field_generator/generate_cube.sh
+++ star_field_generator/generate_cube.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 hs=$[1024*$1]
 
-if [[ ! -f face_1_$hs.png ]]; then 
-	python makemap.py $1
-	convert starmap.png -fill white -pointsize 30  -gravity Center -annotate +0+0 'RA=180,DEC=0' -annotate +$hs+0 'RA=90,DEC=0' -annotate -$hs+0 'RA=270,DEC=0' -gravity West -annotate +0+0 'RA=0,DEC=0' -annotate +0+$[$hs*2/3] 'RA=0,DEC=-60' -annotate +0-$[$hs*2/3] 'RA=0,DEC=60'  starmap.png 
-	sphere2cube starmap.png -R 0 0 0 -t 28 -r $hs -f PNG
-else
-	cp face_4_$hs.png ../camera_simulator/Data_$hs/ft.png
-	cp face_1_$hs.png ../camera_simulator/Data_$hs/rt.png
-	cp face_2_$hs.png ../camera_simulator/Data_$hs/bk.png
-	cp face_3_$hs.png ../camera_simulator/Data_$hs/lt.png
-	cp face_6_$hs.png ../camera_simulator/Data_$hs/dn.png
-	cp face_5_$hs.png ../camera_simulator/Data_$hs/up.png
-	cd ../camera_simulator/
-	rm Data
-	ln -s Data_$hs Data
-fi
+python makemap.py $1
+#convert starmap.png -fill white -pointsize 30  -gravity Center -annotate +0+0 'RA=180,DEC=0' -annotate +$hs+0 'RA=90,DEC=0' -annotate -$hs+0 'RA=270,DEC=0' -gravity West -annotate +0+0 'RA=0,DEC=0' -annotate +0+$[$hs*2/3] 'RA=0,DEC=-60' -annotate +0-$[$hs*2/3] 'RA=0,DEC=60'  starmap.png 
+sphere2cube starmap.png -R 0 0 0 -t 28 -r $hs -f PNG
+cp face_4_$hs.png ~/startracker/camera_simulator/Data_$hs/ft.png
+cp face_1_$hs.png ~/startracker/camera_simulator/Data_$hs/rt.png
+cp face_2_$hs.png ~/startracker/camera_simulator/Data_$hs/bk.png
+cp face_3_$hs.png ~/startracker/camera_simulator/Data_$hs/lt.png
+cp face_5_$hs.png ~/startracker/camera_simulator/Data_$hs/dn.png
+cp face_6_$hs.png ~/startracker/camera_simulator/Data_$hs/up.png
+scp face_4_$hs.png andrew@muri1.eng.buffalo.edu:~/startracker/camera_simulator/Data_$hs/ft.png
+scp face_1_$hs.png andrew@muri1.eng.buffalo.edu:~/startracker/camera_simulator/Data_$hs/rt.png
+scp face_2_$hs.png andrew@muri1.eng.buffalo.edu:~/startracker/camera_simulator/Data_$hs/bk.png
+scp face_3_$hs.png andrew@muri1.eng.buffalo.edu:~/startracker/camera_simulator/Data_$hs/lt.png
+scp face_5_$hs.png andrew@muri1.eng.buffalo.edu:~/startracker/camera_simulator/Data_$hs/dn.png
+scp face_6_$hs.png andrew@muri1.eng.buffalo.edu:~/startracker/camera_simulator/Data_$hs/up.png
